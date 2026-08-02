1347650e8a47b7860de2cfb6fb0e39657c23c343
AysuMustafazade <mustafazadays16@gmail.com>
2026-08-02 19:59:47 +0400

    done

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -15,6 +15,11 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -31,11 +36,34 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 
-const printTeacher: printTeacherFunction = (
-  firstName: string,
-  lastName: string
-): string => `${firstName.charAt(0)}. ${lastName}`;
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(
+    private firstName: string,
+    private lastName: string
+  ) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('John', 'Doe');
 
 console.log(teacher3);
 console.log(director1);
 console.log(printTeacher('John', 'Doe'));
+console.log(student.displayName());
+console.log(student.workOnHomework());
