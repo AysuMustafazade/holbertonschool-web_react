cf6d9b9f21910286380de1116e9ea723416892fb
AysuMustafazade <mustafazadays16@gmail.com>
2026-08-02 19:56:17 +0400

    done

---
+++ task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [attribute: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
