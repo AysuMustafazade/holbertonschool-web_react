0f17829a2c1f40681f3b2c871080cf71fe3be80e
AysuMustafazade <mustafazadays16@gmail.com>
2026-08-02 19:59:08 +0400

    done

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -11,12 +11,31 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
 const director1: Directors = {
   firstName: 'John',
   lastName: 'Doe',
-  location: 'London',
   fullTimeEmployee: true,
+  location: 'London',
   numberOfReports: 17,
 };
 
+const printTeacher: printTeacherFunction = (
+  firstName: string,
+  lastName: string
+): string => `${firstName.charAt(0)}. ${lastName}`;
+
+console.log(teacher3);
 console.log(director1);
+console.log(printTeacher('John', 'Doe'));
